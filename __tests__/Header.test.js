23a9aac1ef717a31dc756fe6a2dff1a5932cae09
Arthur Lehuen <arthur@lehuen.net>
2017-07-03 16:10:26 +0200

    add components

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -8,10 +8,8 @@ import Header from '../components/Header';
 
 describe('<Header />', () => {
 
-    /**
-     * TODO
-     */
-    it('will fail', () => {
-        expect(false).toBe(true)
+    it('has a "<title>" tag equal to Estimeo Highlights', () => {
+      const wrapper = shallow(<Header />);
+      expect(wrapper.find('title').text()).toBe("Estimeo Highlights");
     })
 })
